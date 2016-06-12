baa12728a4ca6ba983ad294311f926109f4d0725
weijie6067 <746497139@qq.com>
2016-06-12 12:28:05 +0800

    6-12

---
+++ 博看移动端/就业详细页/js/zoom.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+	        var wid=document.documentElement.clientWidth;
+			var scale=wid/640;
+			document.body.style.webkitZoom=scale;
+			document.body.style.zoom=scale;
+	window.onresize=function(){
+			var wid=document.documentElement.clientWidth;
+			var scale=wid/640;
+			document.body.style.webkitZoom=scale;
+			document.body.style.zoom=scale;
+			}
+}
